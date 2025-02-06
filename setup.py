11a2b98a1feb2858c51afa184cadecccc9bf8742
Daniel <git@casasc.org>
2025-02-06 18:22:30 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyarchivebox",
+    version="0.0.0",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.32.3"
+    ],
+)
