2036694e49571fa3c96c606b2646dc70f7de0bb0
Daniel <git@casasc.org>
2025-02-07 21:04:37 +0100

    added __get_archiveid method

--- pyarchivebox/main.py
+++ pyarchivebox/main.py
@@ -13,6 +13,26 @@ class PyArchiveBox:
         self.csrf_token = None
         self.session_cookie = None
 
+    def __get_archiveid(self, title: str, date_added: str):
+        session = self.session
+        url = self.url + f"/admin/core/snapshot/?q={title}"
+
+        headers = {
+            "Cookie": f"csrftoken={self.csrf_token}; sessionid={self.session_cookie}; GMT_OFFSET=60"
+        }
+        page = session.get(url, headers=headers)
+        html = HTMLParser(page.text)
+
+        for row in html.css("table#result_list tbody tr"):
+            checkbox = row.css_first("td.action-checkbox input[name='_selected_action']")
+            archive_id = checkbox.attributes["value"] if checkbox else None
+
+            archive_title = row.css_first("td.field-title_str .status-fetched").text(strip=True) if row.css_first("td.field-title_str") else None
+            archive_date = row.css_first("th.field-added").text(strip=True) if row.css_first("th.field-added") else None
+
+            if archive_id and archive_title and archive_date:
+                if archive_title == title and archive_date == date_added:
+                    return archive_id
 
     def login(self):
         login_url = self.url + "/admin/login/?next=/"
@@ -69,28 +89,17 @@ class PyArchiveBox:
     def delete(self, title: str, date_added: str):
         session = self.session
         snapshots_url = self.url + f"/admin/core/snapshot/?q={title}"
-
+        archive_id = self.__get_archiveid(title, date_added)
+        
         headers = {
             "Cookie": f"csrftoken={self.csrf_token}; sessionid={self.session_cookie}; GMT_OFFSET=60"
         }
-        page = session.get(snapshots_url, headers=headers)
-        html = HTMLParser(page.text)
-
-        for row in html.css("table#result_list tbody tr"):
-            checkbox = row.css_first("td.action-checkbox input[name='_selected_action']")
-            archive_id = checkbox.attributes["value"] if checkbox else None
-
-            archive_title = row.css_first("td.field-title_str .status-fetched").text(strip=True) if row.css_first("td.field-title_str") else None
-            archive_date = row.css_first("th.field-added").text(strip=True) if row.css_first("th.field-added") else None
-
-            if archive_id and archive_title and archive_date:
-                if archive_title == title and archive_date == date_added:
-                    payload = {
-                        "csrfmiddlewaretoken": f"{self.csrf_token}",
-                        "action": "delete_snapshots",
-                        "select_across": 0,
-                        "index": 0,
-                        "_selected_action": f"{archive_id}"
-                    }
-                    response = session.post(snapshots_url, data=payload, headers=headers)
-                    return response
+        payload = {
+            "csrfmiddlewaretoken": f"{self.csrf_token}",
+            "action": "delete_snapshots",
+            "select_across": 0,
+            "index": 0,
+            "_selected_action": f"{archive_id}"
+        }
+        response = session.post(snapshots_url, data=payload, headers=headers)
+        return response
