b1c485aa4eeef45ecb8608bed78b1f30bed069d4
Daniel <git@casasc.org>
2025-02-08 16:37:40 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,23 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyarchivebox",
-    version="0.0.0",
+    version="0.0.1",
+    description="A simpler way to interact with your archivebox server via python",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/danielscrb/pyarchivebox",
+    author="danielscrb",
+    author_email="danielscrb@casasc.org",
+    license="MIT-Modified",
     packages=find_packages(),
-    install_requires=[
-        "requests>=2.32.3"
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Operating System :: OS independent"
     ],
+    install_requires=[
+        "requests>=2.32.3",
+        "selectolax>=0.3.27"
+    ],
+    python_requires=">=3.10"
 )
