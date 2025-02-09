2757c7977acc2cf6a8c2a201e25d99a12acfb378
Daniel <git@casasc.org>
2025-02-09 12:09:07 +0100

    added __get_latest_archives method

--- pyarchivebox/main.py
+++ pyarchivebox/main.py
@@ -32,6 +32,40 @@ class PyArchiveBox:
             if archive_id and archive_title and archive_date:
                 if archive_title == title and archive_date == date_added:
                     return archive_id
+                
+
+    def __get_latest_archives(self):
+        session = self.session
+        url = self.url + f"/admin/core/snapshot/"
+
+        headers = {
+            "Cookie": f"csrftoken={self.csrf_token}; sessionid={self.session_cookie}; GMT_OFFSET=60"
+        }
+        page = session.get(url, headers=headers)
+        html = HTMLParser(page.text)
+        archives = {}
+        
+        counter = 0
+
+        for row in html.css("table#result_list tbody tr"):
+            archive_title = row.css_first("td.field-title_str .status-fetched").text(strip=True) if row.css_first("td.field-title_str .status-fetched") else None
+            archive_date = row.css_first("th.field-added").text(strip=True) if row.css_first("th.field-added") else None
+            archive_url = row.css_first("td.field-url_str code").text(strip=True) if row.css_first("td.field-url_str code") else None
+            archive_size = row.css_first("td.field-size b").text(strip=True) if row.css_first("td.field-size b") else None
+
+            archives.update({
+                counter: {
+                    "title": archive_title,
+                    "date_added": archive_date,
+                    "url": archive_url,
+                    "size": archive_size
+                }
+            })
+
+            counter += 1
+
+        return archives
+
 
     def login(self):
         login_url = self.url + "/admin/login/?next=/"
